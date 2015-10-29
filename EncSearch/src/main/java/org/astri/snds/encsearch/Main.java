19d98c31d7d884ed48ca1f99b4ea480d16eb9af9
Roman Plášil <me@rplasil.name>
2015-10-29 18:35:08 +0800

    Fix #2, fix one character in Main.java

--- EncSearch/src/main/java/org/astri/snds/encsearch/Main.java
+++ EncSearch/src/main/java/org/astri/snds/encsearch/Main.java
@@ -102,7 +102,7 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			if (args.length < 1) {
-				System.out.println("init | add | q");
+				System.out.println("init | add | q");
 				return;
 			}
 			String op = args[0];
